b546093aaf7a84bd51ee9667cfcbb206879b30f5
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-01 15:22:36 +0530

    Create html.render.ts
    Template render for processing string templates

---
+++ core/render/html.render.ts
@@ -0,0 +1,9 @@
+
+
+export function HTML(strings: TemplateStringsArray, ...values: any[]){
+    let str = '';
+    strings.forEach((string, i) => {
+        str += string + (values[i] || '');
+    });
+    return str;
+}
