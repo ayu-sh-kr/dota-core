d971a97fdde74e6936e51f0f9e1a777f6b69c729
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-01 15:22:54 +0530

    Create index.ts
    Entry point for the project

---
+++ index.ts
@@ -0,0 +1,4 @@
+export {Component, type ComponentConfig} from "./core/decorators/component.decorator";
+export {Property, type PropertyConfig} from "./core/decorators/property.decorator";
+export {BaseElement} from "./core/elements/base-elements";
+export {HTML} from "./core/render/html.render";
