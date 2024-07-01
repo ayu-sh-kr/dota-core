2e4c933cf36e503768fb6a4e83d512b9cb9ce6eb
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-01 15:34:01 +0530

    Added JS docs for the ComponentConfig and build configuration in package.json

--- core/decorators/component.decorator.ts
+++ core/decorators/component.decorator.ts
@@ -1,4 +1,11 @@
-
+/**
+ * Configuration object for the Component decorator.
+ *  @type {Object} ComponentConfig
+ *  @prop {string} [selecter = ''] - The custom element name to define.
+ *  @prop {boolean} [shadow = false] - Optional.
+ *  Determines whether the component should use a shadow root.
+ *  Defaults to false.
+ */
 
 export interface ComponentConfig {
     selector: string;
