20c57c910a1e753fdb3655cacbc45bac50b5b5de
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-03 01:58:47 +0530

    Update @Component: Fix it to enable shadow dom if provided

--- core/decorators/component.decorator.ts
+++ core/decorators/component.decorator.ts
@@ -20,22 +20,34 @@ export interface ComponentConfig {
  * @param config
  * @constructor
  */
-export function Component(config: ComponentConfig): ClassDecorator{
+export function Component(config: ComponentConfig) {
     return function (target: any) {
 
-        // Get the names of the methods in the target class
+        /* Get the names of the methods in the target class */
         const methodNames = Object.getOwnPropertyNames(target.prototype)
             .filter(prop => typeof target.prototype[prop] === 'function' && prop !== 'constructor');
 
-        // Create an array of MethodDetails objects
+        /**
+         *  @Create an array of MethodDetails objects
+         *
+         */
         const methods: MethodDetails[] = methodNames.map(name => ({
             name: name,
             method: target.prototype[name],
         }));
 
-        // Store the methods in the metadata
+        /**
+         *  @Store the methods in the metadata
+         */
         Reflect.defineMetadata(target.name, methods, target);
 
-        customElements.define(config.selector, target);
+        /**
+         *  @Register the Component
+         */
+        if (!customElements.get(config.selector)) {
+            customElements.define(config.selector, target);
+        }
+
+        Reflect.defineMetadata(target.name + ':' + 'shadow', config.shadow, target);
     }
 }
