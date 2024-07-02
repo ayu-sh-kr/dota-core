036af20ff40515d139eea77461046523a4267413
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-02 17:23:58 +0530

    Update index.ts: exported type MethodDetails

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export {Property, type PropertyConfig} from "./core/decorators/property.decorato
 export {BaseElement} from "./core/elements/base-elements";
 export {HTML} from "./core/render/html.render";
 export {EventListener} from "./core/decorators/event-listener.decorator";
+export {type MethodDetails} from "./core/types/core.types";
