68b9dfa311723fbaa21dff85176880b97d6f393e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-03 14:30:02 +0530

    Update index.ts: Refactoring and export all the content from here

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-export {Component, type ComponentConfig} from "./core/decorators/component.decorator";
-export {Property, type PropertyConfig} from "./core/decorators/property.decorator";
+export {Component} from "./core/decorators/component.decorator";
+export {Property} from "./core/decorators/property.decorator";
 export {BaseElement} from "./core/elements/base-elements";
 export {HTML} from "./core/render/html.render";
 export {EventListener} from "./core/decorators/event-listener.decorator";
-export {type MethodDetails} from "./core/types/core.types";
+export {BindEvent} from "./core/decorators/bind-event.decorators";
+export type {MethodDetails, BindConfig, PropertyConfig, EventConfig, ComponentConfig} from "./core/types/core.types";
