d0c09e7b4942680e3bbef5c301848a357632ba30
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-03 14:28:15 +0530

    Create bind-event.decorator.ts: Decorator to bind event with methods

---
+++ core/decorators/bind-event.decorators.ts
@@ -0,0 +1,19 @@
+import {BindConfig} from "@dota/types/core.types.ts";
+
+
+export function BindEvent(config: BindConfig): MethodDecorator {
+    return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+        let key: string = `${target.constructor.name}:Bind`;
+        let data: Map<string, BindConfig>;
+
+        if(!Reflect.getMetadata(key, target.constructor)) {
+            data = new Map<string, BindConfig>();
+            Reflect.defineMetadata(key, data, target.constructor);
+        }
+
+        data = Reflect.getMetadata(key, target.constructor);
+        data.set(propertyKey.toString(), config);
+
+        return descriptor;
+    }
+}
