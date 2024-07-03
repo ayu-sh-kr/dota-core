d2cf72f653b70442763350655ae5fd4d8a6a2b3b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-03 14:27:14 +0530

    Update core.types.ts: Moved All Config type to this file

--- core/types/core.types.ts
+++ core/types/core.types.ts
@@ -1,6 +1,36 @@
 
 
+/**
+ * Configuration object for the Component decorator.
+ *  @type {Object} ComponentConfig
+ *  @prop {string} [selecter = ''] - The custom element name to define.
+ *  @prop {boolean} [shadow = false] - Optional.
+ *  Determines whether the component should use a shadow root.
+ *  Defaults to false.
+ */
+
+export interface ComponentConfig {
+    selector: string;
+    shadow?: boolean
+}
+
 export interface MethodDetails {
     name: string;
     method: Function
+}
+
+export interface BindConfig {
+    event: string,
+    id: string,
+    params?: any[]
+}
+
+export interface PropertyConfig {
+    name: string;
+    default?: string
+}
+
+export interface EventConfig {
+    name: string;
+    options?: EventListenerOptions
 }
