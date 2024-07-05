3762dd47650e5975819bb649ac4e9aa838987955
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-05 13:45:45 +0530

    Fix export of base element

---
+++ .changeset/fresh-clouds-sing.md
@@ -0,0 +1,5 @@
+---
+"@ayu-sh-kr/dota-core": patch
+---
+
+exported the BaseElement class
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export {Property} from "@dota/core/decorators/property.decorator.ts";
 export {EventListener} from "@dota/core/decorators/event-listener.decorator.ts";
 export {BindEvent} from "@dota/core/decorators/bind-event.decorators.ts";
 export {Expose} from "@dota/core/decorators/expose.decorator.ts";
+export {BaseElement} from "@dota/core/elements/base-elements.ts";
 export type {PropertyConfig, EventConfig, PropertyDetails, BindConfig, MethodDetails, ComponentConfig} from "@dota/core/types/core.types.ts";
