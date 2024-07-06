31f2b2931cd5a6b09e35433b86cd7a98851dc3d2
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-06 16:42:15 +0530

    Update index.ts: Move to SRC

--- index.ts
+++
@@ -1,7 +0,0 @@
-export {Component} from "./core/decorators/component.decorator";
-export {Property} from "./core/decorators/property.decorator";
-export {BaseElement} from "./core/elements/base-elements";
-export {HTML} from "./core/render/html.render";
-export {EventListener} from "./core/decorators/event-listener.decorator";
-export {BindEvent} from "./core/decorators/bind-event.decorators";
-export type {MethodDetails, BindConfig, PropertyConfig, EventConfig, ComponentConfig, PropertyDetails} from "./core/types/core.types";
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export {EventListener} from "@dota/core/decorators/event-listener.decorator.ts";
 export {BindEvent} from "@dota/core/decorators/bind-event.decorators.ts";
 export {Expose} from "@dota/core/decorators/expose.decorator.ts";
 export {BaseElement} from "@dota/core/elements/base-elements.ts";
+export {HTML} from "@dota/core/render/html.render.ts";
 export type {PropertyConfig, EventConfig, PropertyDetails, BindConfig, MethodDetails, ComponentConfig} from "@dota/core/types/core.types.ts";
