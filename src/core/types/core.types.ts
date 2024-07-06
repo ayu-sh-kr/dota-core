9025b8e63bde94d4ed1dcfbcba88a54b5b661b75
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-06 16:43:02 +0530

    Update core.types.ts
    Added docs for Types

--- src/core/types/core.types.ts
+++ src/core/types/core.types.ts
@@ -14,27 +14,61 @@ export interface ComponentConfig {
     shadow?: boolean
 }
 
+
+/**
+ * Configuration details of methods for Method Decorators
+ * @type {Object} MethodDetails
+ * @prop {string} name - name of the method
+ * @prop {Function} method - method itself
+ */
 export interface MethodDetails {
     name: string;
     method: Function
 }
 
+
+/**
+ * Configuration details for Binding Element Events to internal methods through BindEvent decorator {@BindEvents}
+ * @type{Object}
+ * @prop {string} event - name of the event
+ * @prop {string} id - id of the element to bind event on
+ * @prop {string} params - params required by the method (optional)
+ */
 export interface BindConfig {
     event: string,
     id: string,
     params?: any[]
 }
 
+
+/**
+ * Configuration details for Binding Property with Attribute through Property decorator {@Property}
+ * @prop {string} name - name of the attribute
+ * @prop {string} default - default value for attribute
+ */
 export interface PropertyConfig {
     name: string;
     default?: string
 }
 
+
+/**
+ * Configuration details for Binding Component Level events through Event Listener decorator {@EventListner}
+ * @prop {string} name - name of the event
+ * @prop {EventListenerOptions} options - option for extra configuration (optional && not available at the moment)
+ */
 export interface EventConfig {
     name: string;
     options?: EventListenerOptions
 }
 
+
+/**
+ * Storage object for storing Property Details for binding property upon connectedCallback
+ * @prop {string} name - attribute name of property
+ * @prop {string} prototype - property name in the class
+ * @prop {default} default - default value of the property (optional)
+ */
 export interface PropertyDetails {
     name: string
     prototype: string
