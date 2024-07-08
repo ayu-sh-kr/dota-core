41debe5a7774c55922c348e565fba2fc2f961431
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-08 21:25:18 +0530

    Update index.ts: exported decorator AfterInit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {Property} from "@dota/core/decorators/property.decorator.ts";
 export {EventListener} from "@dota/core/decorators/event-listener.decorator.ts";
 export {BindEvent} from "@dota/core/decorators/bind-event.decorators.ts";
 export {Expose} from "@dota/core/decorators/expose.decorator.ts";
+export {AfterInit} from "@dota/core/decorators/after-init.decorator.ts";
 export {BaseElement} from "@dota/core/elements/base-elements.ts";
 export {HTML} from "@dota/core/render/html.render.ts";
 export type {PropertyConfig, EventConfig, PropertyDetails, BindConfig, MethodDetails, ComponentConfig} from "@dota/core/types/core.types.ts";
