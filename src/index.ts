da47d2d2750ee8e102d9d641af78214e9bfc7581
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-14 12:02:35 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export {Expose} from "@dota/core/decorators/expose.decorator.ts";
 export {AfterInit} from "@dota/core/decorators/after-init.decorator.ts";
 export {BaseElement} from "@dota/core/elements/base-elements.ts";
 export {HTML} from "@dota/core/render/html.render.ts";
+export {EventEmitter} from "@dota/core/utils/EventEmitter.ts";
 export type {PropertyConfig, EventConfig, PropertyDetails, BindConfig, MethodDetails, ComponentConfig} from "@dota/core/types/core.types.ts";
