9289591552ffa3b8848ae50e825166fe0757975d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-25 01:13:43 +0530

    Update index.ts: fix the missing export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "@dota/core/decorators/app.decorator.ts";
 export {HTML} from "@dota/core/render/html.render.ts";
 export * from "@dota/core/types/core.types.ts";
+export {BaseElement} from "@dota/core/elements/base-elements.ts";
+export {EventEmitter} from "@dota/core/utils/EventEmitter.ts";
