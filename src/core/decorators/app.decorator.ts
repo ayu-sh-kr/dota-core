1a61dcf9bbf09887aece9fdab8fa4a91ff1c4746
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-25 01:04:49 +0530

    Create app.decorator.ts: Code optimization and remove load from index.ts

---
+++ src/core/decorators/app.decorator.ts
@@ -0,0 +1,19 @@
+import { AfterInit } from '@dota/core/decorators/after-init.decorator.ts';
+import { BeforeInit } from '@dota/core/decorators/before-init.decorator.ts';
+import { BindEvent } from '@dota/core/decorators/bind-event.decorators.ts';
+import { Component } from '@dota/core/decorators/component.decorator.ts';
+import { Property } from '@dota/core/decorators/property.decorator.ts';
+import { EventListener } from '@dota/core/decorators/event-listener.decorator.ts';
+import { Expose } from '@dota/core/decorators/expose.decorator.ts';
+import { Event } from '@dota/core/decorators/event.decorator.ts';
+
+export {
+    AfterInit,
+    BeforeInit,
+    BindEvent,
+    Component,
+    Property,
+    EventListener,
+    Expose,
+    Event
+};
